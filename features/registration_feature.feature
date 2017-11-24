b235fbf5f6b456e668ca93bbe5cfd446ed0bcae3
aljoh <reachingalfred@gmail.com>
2017-11-24 12:10:32 +0100

    refactored tests for registration page

--- features/registration_feature.feature
+++ features/registration_feature.feature
@@ -3,33 +3,25 @@ Feature: Registration page
   In order to login
   I need to be able to register
 
-  Scenario: Visitor sees a form to register on register page
-    When I visit the register page
-    Then I would like to see a form for registration
-    And I would like to see a text field for user name
-    And I would like to see a text field for password
-    And I would like to see a text field for password confirmation
-    And I would like to see a sign up button
-
   Scenario: Visitor writes his information and presses register
     Given I visit the register page
-    When I fill in 'name' with 'alfred'
-    And I fill in 'password' with 'whatever'
-    And I fill in 'password confirmation' with 'whatever'
+    When I fill in "username" with "Alfred"
+    And I fill in "password" with "whatever"
+    And I fill in "confirm_password" with "whatever"
     When I press the sign up button
     Then I would like my information to be saved
-    And I would like a message saying my user has been created
+    And I get a message saying "Your user has been created"
     And I would like to be redirected to the home page
 
   Scenario: Visitor gets error message when not filling in password
     Given I visit the register page
-    When I fill in 'name' with 'alfred'
+    When I fill in "username" with "Alfred"
     And I press the sign up button
-    Then I get en error message saying you need to enter a password
+    Then I get a message saying "Password can't be blank"
 
   Scenario: Visitor gets error message when password confirmation is different as password
     Given I visit the register page
-    When I fill in 'password' with 'whatever'
-    And I fill in 'password confirmation' with 'poop'
+    When I fill in "password" with "whatever"
+    And I fill in "confirm_password" with "poop"
     And I press the sign up button
-    Then I get an error message saying password confirmation differs from password
+    Then I get a message saying "Password confirmation doesn't match Password"
--- features/step_definitions/register_page_steps.rb
+++ features/step_definitions/register_page_steps.rb
@@ -1,41 +1,9 @@
-When(/^I visit the register page$/) do
+Given(/^I visit the register page$/) do
   visit '/signup'
 end
 
-Then(/^I would like to see a form for registration$/) do
-  expect(page).to have_selector 'form'
-end
-
-Then(/^I would like to see a text field for user name$/) do
-  expect(page).to have_field 'username'
-end
-
-Then(/^I would like to see a text field for password$/) do
-  expect(page).to have_field 'password'
-end
-
-Then(/^I would like to see a text field for password confirmation$/) do
-  expect(page).to have_field 'confirm_password'
-end
-
-Then(/^I would like to see a sign up button$/) do
-  expect(page).to have_selector(:link_or_button, "Sign up")
-end
-
-Given(/^there is a register page$/) do
-  visit '/signup'
-end
-
-When(/^I fill in 'name' with 'alfred'$/) do
-    fill_in 'username', :with => 'alfred'
-end
-
-When(/^I fill in 'password' with 'whatever'$/) do
-  fill_in 'password', :with => 'whatever'
-end
-
-When(/^I fill in 'password confirmation' with 'whatever'$/) do
-  fill_in 'confirm_password', :with => 'whatever'
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field_name, value|
+  fill_in field_name, with: value
 end
 
 When(/^I press the sign up button$/) do
@@ -44,35 +12,13 @@ end
 
 Then(/^I would like my information to be saved$/) do
   user = User.last
-  expect(user.name).to eq 'alfred'
+  expect(user.name).to eq 'Alfred'
 end
 
-Then(/^I would like a message saying my user has been created$/) do
-  expect(page).to have_content 'Your user has been created'
+Then(/^I get a message saying "([^"]*)"$/) do |message|
+  expect(page).to have_content message
 end
 
 Then(/^I would like to be redirected to the home page$/) do
   expect(page).to have_current_path '/'
-end
-
-
-Given(/^only the username has been entered$/) do
-  fill_in 'username', :with => 'alfred'
-end
-
-Then(/^I get en error message saying you need to enter a password$/) do
-  expect(page).to have_content "Password can't be blank"
-end
-
-Given(/^the password confirmation differs from password$/) do
-  fill_in 'username', :with => 'alfred'
-  fill_in 'password', :with => 'whatever'
-end
-
-When(/^I fill in 'password confirmation' with 'poop'$/) do
-  fill_in 'confirm_password', :with => 'poop'
-end
-
-Then(/^I get an error message saying password confirmation differs from password$/) do
-  expect(page).to have_content "Password confirmation doesn't match Password"
 end
